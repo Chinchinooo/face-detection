e7eea6786b9eca59bb03267cdb2573b15c8a3870
Chinchinooo <chinchin081298@gmail.com>
2024-01-31 17:32:08 +0800

    Done w ImageLinkForm and moving to Rank

--- src/component/ImageLinkForm/ImageLinkForm.js
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
     return (
         <div>
-            <p>
-            Miao
-            </p>
+            <div className="center">
+                <p className='f3 white'>
+                Miao can detect hooman faces. Try me.
+                </p>
+            </div>
+            <div className="center">
+                <div className="form center pa4 ba br1 white">
+                   <input className="f4 pa2 w-70 center" type='text' placeholder="Gimme pics now"/>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-transparent ba br1 b--white">Detect</button> 
+                </div>
+            </div>
         </div>
     );
 }
