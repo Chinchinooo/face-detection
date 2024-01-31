9ff79421f1b483db96566fd5a90f8166bed1e5cd
Chinchinooo <chinchin081298@gmail.com>
2024-01-31 17:44:00 +0800

    created folder rank

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
+import Rank from './component/Rank/Rank';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <ImageLinkForm />
+        <Rank />
         {/* <FaceRecognition /> */}
       </div>
     )
---
+++ src/component/Rank/Rank.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        "hi"
+    );
+}
+
+export default Rank;
