172b8c5ad54cf142d5a1e7a28a70bfd5a937e607
Chinchinooo <chinchin081298@gmail.com>
2024-02-01 21:01:20 +0800

    button detect chg grow to dim

--- src/component/ImageLinkForm/ImageLinkForm.js
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -12,7 +12,7 @@ const ImageLinkForm = () => {
             <div className="center">
                 <div className="form center pa4 ba br1 white">
                    <input className="f4 pa2 w-70 center" type='text' placeholder="Gimme pics now"/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-transparent ba br1 b--white">Detect</button> 
+                    <button className="w-30 dim f4 link ph3 pv2 dib white bg-transparent ba br1 b--white">Detect</button> 
                 </div>
             </div>
         </div>
