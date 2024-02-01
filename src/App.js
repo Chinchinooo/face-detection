732396718c417409aa41238c526f7af46b8b14c8
Chinchinooo <chinchin081298@gmail.com>
2024-02-01 19:20:32 +0800

    added particle and moving on to custom the setting

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 import Rank from './component/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <ParticlesBg color="#ff0000" num={80} type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
