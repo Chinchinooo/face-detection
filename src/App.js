0d0cb5e65c50cb123c3685fd8d5f7ee7fd74ceb5
Chinchinooo <chinchin081298@gmail.com>
2024-02-04 14:53:56 +0800

    adding component FaceRecognition

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 import Rank from './component/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import './App.css';
+import { click } from '@testing-library/user-event/dist/click';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onSubmit = () => {
+    console.log('click')
+  }
+  
+  
   render () {
     return (
       <div className="App">
@@ -14,8 +31,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+        <FaceRecognition />
       </div>
     )
   };
--- src/component/ImageLinkForm/ImageLinkForm.js
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit}) => {
     return (
         <div>
             <div className="center">
@@ -11,8 +11,11 @@ const ImageLinkForm = () => {
             </div>
             <div className="center">
                 <div className="form center pa4 ba br1 white">
-                   <input className="f4 pa2 w-70 center" type='text' placeholder="Gimme pics now"/>
-                    <button className="w-30 dim f4 link ph3 pv2 dib white bg-transparent ba br1 b--white">Detect</button> 
+                   <input className="f4 pa2 w-70 center" type='text' placeholder="Gimme pics now"
+                   onChange={onInputChange}/>
+                    <button className="w-30 dim f4 link ph3 pv2 dib white bg-transparent ba br1 b--white"
+                     onClick={onSubmit}
+                     >Detect</button> 
                 </div>
             </div>
         </div>
