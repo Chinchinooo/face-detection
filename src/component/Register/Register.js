8f4bd8ad4a5daaa086b3b442070eca270348f2e8
Chinchinooo <chinchin081298@gmail.com>
2024-02-16 01:03:58 +0800

    fixed class to classname, for to htmlfor, fix center sign in

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Register = ({onRouteChange}) => {
   return (
-    <article class="br2 ba dark-gray b--white-50 mv4 w-100 w-50-m w-25-l mw5 center">
+    <article className="br2 ba dark-gray b--white-50 mv4 w-100 w-50-m w-25-l mw5 center">
         <main className="pa4 black-80">
             <form className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0 white">
                 <legend className="f3 fw6 ph0 mh0">Register</legend>
-                <div class="mt3">
-                    <label className="db fw6 lh-copy f6" for="name">Name</label>
+                <div className="mt3">
+                    <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="email-address"/>
                 </div>
-                <div class="mt3">
-                    <label className="db fw6 lh-copy f6" for="email-address">Email address</label>
+                <div className="mt3">
+                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email address</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                 </div>
                 <div className="mv3">
-                    <label className="db fw6 lh-copy f6" for="password">Password</label>
+                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                     <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                 </div>
                 </fieldset>
