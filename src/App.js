550b384a28bab930921e7bc79604dfb019a1ff87
Chinchinooo <chinchin081298@gmail.com>
2024-02-15 16:11:48 +0800

    sign in component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SignIn from './component/SignIn/SignIn';
 import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
@@ -45,9 +46,28 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
-      
+      imageUrl: '',
+      box: {},
+      route: 'signin'
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -59,42 +79,25 @@ class App extends Component {
 
     fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", setUpApi(this.state.input))
     .then(response => response.json())
-    .then(result => {
-
-        const regions = result.outputs[0].data.regions;
-
-        regions.forEach(region => {
-            // Accessing and rounding the bounding box values
-            const boundingBox = region.region_info.bounding_box;
-            const topRow = boundingBox.top_row.toFixed(3);
-            const leftCol = boundingBox.left_col.toFixed(3);
-            const bottomRow = boundingBox.bottom_row.toFixed(3);
-            const rightCol = boundingBox.right_col.toFixed(3);
-
-            region.data.concepts.forEach(concept => {
-                // Accessing and rounding the concept value
-                const name = concept.name;
-                const value = concept.value.toFixed(4);
-
-                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-                
-            });
-        });
-
-    })
+    .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
     .catch(error => console.log('error', error));
   }
-  
+
   render () {
     return (
       <div className="App">
         <ParticlesBg color="#ff0000" num={80} type="cobweb" bg={true} />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl} />
-      </div>
+        { this.state.route === 'signin'
+          ? <SignIn/>
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+        }
+        </div>
     )
   };
 }
--- src/component/FaceRecognition/FaceRecognition.js
+++ src/component/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './FaceRecognition.css'
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
-    return (
-        <div className='center pa2 display'>
-           <img src={imageUrl} alt=''/>
-        </div>
-    );
+const FaceRecognition = ({ imageUrl, box }) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageUrl} width='' height='500'/>
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
+    </div>
+  );
 }
 
 export default FaceRecognition;
